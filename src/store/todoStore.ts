78683d5394e2ce736da7efe56e7eaa8837f6956c
yujinimda <s0912135@gmail.com>
2025-03-14 01:06:08 +0900

    feat: 회원의 todo 데이터 넣기

--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -1,9 +1,11 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
+import { collection, addDoc, getDocs, doc, deleteDoc } from "firebase/firestore";
+import { db, auth } from "../lib/firebaseConfig";
 
 // Todo 타입 정의
 export type Todo = {
-  id: number;
+  id?: number; // id에서 id? 로 수정 왜냐하면 원래 이거를 로컬에서는 날짜로 넣었는데 파이어베이스에서는 비어져있다가 처음 랜덤 생성되는 아이디가 들어갈꺼라서 
   title: string;
   content: string;
   completed: boolean;
@@ -57,33 +59,74 @@ export const useTodoStore = create<TodoStore>()(
       setDate: (date) => set({ date }),
       setEditingId: (id) => set({ isEditingId: id }),
 
-      addTodo: (title, content, date) => {
-        set((state) => ({
-          todos: [
-            ...state.todos,
-            {
-              id: Date.now(),
-              title,
-              content:content.trim() ? content: "\u00A0",
-              completed: false,
-              isImportant: false,
-              date,
-            },
-          ],
-        }));
+
+      // 비회원일때 로컬만 회원일땐 파이어베이스에서 가져오기
+      fetchTodos: async () => {
+        const user = auth.currentUser;
+        if (user) {
+          const querySnapshot = await getDocs(collection(db, "users", user.uid, "todos"));
+          const todos = querySnapshot.docs.map((doc) => ({
+            id: doc.id.toString(),
+            ...(doc.data() as Todo), // doc.data()의 반환 타입이 unknown이여서 일단 Todo타입으로 캐스팅 
+          })) as Todo[];
+
+          set({ todos });
+        }
+      },
+
+      addTodo: async (title, content, date) => {
+        const user = auth.currentUser;
+
+        const newTodo = {
+          id="",
+          title,
+          content: content.trim() ? content : "\u00A0",
+          completed: false,
+          isImportant: false,
+          date,
+        };
+      
+
+        if(user) {
+          const docRef = await addDoc(collection(db, "users", user.uid, "todos"), newTodo);
+          set((state) => ({
+            todos: [...state.todos, { ...newTodo, id: docRef.id }],
+          }));
+        } else {
+          set((state) => ({
+            todos: [
+              ...state.todos,
+              {
+                id: Date.now(),
+                title,
+                content:content.trim() ? content: "\u00A0",
+                completed: false,
+                isImportant: false,
+                date,
+              },
+            ],
+          }));
+        }
+        
       },
       
-      deleteTodo: (id) => {
-        set((state) => {
-          const updatedTodos = state.todos.filter((todo) => todo.id !== id);
-      
-          return {
-            todos: updatedTodos,
-            filteredTodos: state.isFiltered
-              ? updatedTodos.filter((todo) => todo.isImportant)
-              : [],
-          };
-        });
+      deleteTodo: async  (id) => {
+        const user = auth.currentUser;
+        if(user) {
+          await deleteDoc(doc(db, "users", user.uid, "todos", id.toString()));
+        } else {
+          set((state) => {
+            const updatedTodos = state.todos.filter((todo) => todo.id !== id);
+        
+            return {
+              todos: updatedTodos,
+              filteredTodos: state.isFiltered
+                ? updatedTodos.filter((todo) => todo.isImportant)
+                : [],
+            };
+          });
+        }
+       
       },
 
       endTodo: (id) => {
@@ -162,4 +205,129 @@ export const useTodoStore = create<TodoStore>()(
     }
   )
 );
+
+//////////////////////////////////////////////////////////////////
+// export const useTodoStore = create<TodoStore>()(
+//   persist(
+//     (set, get) => ({
+//       todos: [],
+//       filteredTodos: [],
+//       isFiltered: false,
+
+//       title: "",
+//       content: "",
+//       date: "",
+//       isEditingId: null,
+
+//       setTitle: (title) => set({ title }),
+//       setContent: (content) => set({ content }),
+//       setDate: (date) => set({ date }),
+//       setEditingId: (id) => set({ isEditingId: id }),
+
+//       addTodo: (title, content, date) => {
+//         set((state) => ({
+//           todos: [
+//             ...state.todos,
+//             {
+//               id: Date.now(),
+//               title,
+//               content:content.trim() ? content: "\u00A0",
+//               completed: false,
+//               isImportant: false,
+//               date,
+//             },
+//           ],
+//         }));
+//       },
+      
+//       deleteTodo: (id) => {
+//         set((state) => {
+//           const updatedTodos = state.todos.filter((todo) => todo.id !== id);
+      
+//           return {
+//             todos: updatedTodos,
+//             filteredTodos: state.isFiltered
+//               ? updatedTodos.filter((todo) => todo.isImportant)
+//               : [],
+//           };
+//         });
+//       },
+
+//       endTodo: (id) => {
+//         set((state) => {
+//           const updatedTodos = state.todos.map((todo) =>
+//             todo.id === id ? { ...todo, completed: !todo.completed } : todo
+//           );
+
+//           return {
+//             todos: updatedTodos,
+//             filteredTodos: state.isFiltered
+//               ? updatedTodos.filter((todo) => todo.isImportant)
+//               : [],
+//           };
+//         });
+//       },
+
+//       editTodo: (id, newTitle, newText, newDate) => {
+//         set((state) => {
+//           const updatedTodos = state.todos.map((todo) =>
+//             todo.id === id
+//               ? { ...todo, title: newTitle, content: newText, date: newDate }
+//               : todo
+//           );
+      
+//           return {
+//             todos: updatedTodos,
+//             filteredTodos: state.isFiltered
+//               ? updatedTodos.filter((todo) => todo.isImportant)
+//               : [],
+//           };
+//         });
+//       },
+
+//       allDeleteTodo: () =>
+//         set(() => ({
+//           todos: [],
+//           filteredTodos: [],
+//           isFiltered: false,
+//         })),
+
+//       importantToggle: (id) => {
+//         set((state) => {
+//           const updatedTodos = state.todos.map((todo) =>
+//             todo.id === id ? { ...todo, isImportant: !todo.isImportant } : todo
+//           );
+      
+//           // 필터 적용 여부에 따라 중요 리스트 업데이트
+//           return {
+//             todos: updatedTodos,
+//             filteredTodos: state.isFiltered
+//               ? updatedTodos.filter((todo) => todo.isImportant)
+//               : [],
+//           };
+//         });
+//       },
+        
+//       showImportantTodos: () => {
+//         const { todos } = get();
+//         set({
+//           filteredTodos: todos.filter((todo) => todo.isImportant),
+//           isFiltered: true,
+//         });
+//       },
+
+//       showAllTodos: () => {
+//         set({
+//           filteredTodos: [],
+//           isFiltered: false,
+//         });
+//       },
+//     }),
+//     {
+//       name: "todo-storage",
+//       storage: createJSONStorage(() => localStorage),
+//     }
+//   )
+// );
+
 
